819a4b54d99bd0aed87109fe5d64f58b5b4a78a3
RezaHarahap <114418673+RezaHarahap@users.noreply.github.com>
2026-09-06 19:31:56 +0700

    feat: add create buffer exercise

---
+++ 08-1-create-buffer/app.js
@@ -0,0 +1,10 @@
+const { Buffer } = require('buffer');
+
+/**
+ * @TODO
+ * 1. Buatlah variabel bernama `buffer` yang merupakan instance dari Buffer dengan panjang 50 byte.
+ */
+
+const buffer = Buffer.alloc(50);
+
+module.exports = buffer;
